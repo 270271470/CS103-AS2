dfd3c54a913257a539b2b97651088116f284a327
JM <200871010@yoobeestudent.ac.nz>
2023-06-09 11:58:51 +1200

    added a mani menu

--- CS103-AS2/CS103-AS2.cpp
+++ CS103-AS2/CS103-AS2.cpp
@@ -2,10 +2,15 @@
 
 #include <iostream>
 #include <iomanip>
+#include <stdlib.h>
 
-int main() {
+void clear() {
+    system("cls");
+}
+
+void login() {
     char userChoice;
-    const int consoleWidth = 80; // Assuming console is 80 characters wide
+    const int consoleWidth = 120; // Assuming console is 120 characters wide
 
     std::string welcomeText = "Welcome to our School Lunch Ordering System!";
     int padding = (consoleWidth - welcomeText.length()) / 2;
@@ -15,7 +20,7 @@ int main() {
     padding = (consoleWidth - roleText.length()) / 2;
     std::cout << std::setw(padding) << "" << roleText << std::endl;
 
-    std::string studentText = "Press 'k' for Student";
+    std::string studentText = "Press 'S' for Student";
     padding = (consoleWidth - studentText.length()) / 2;
     std::cout << std::setw(padding) << "" << studentText << std::endl;
 
@@ -41,5 +46,38 @@ int main() {
         break;
     }
 
-    return 0;
+}
+
+void ViewDiscounts() {
+
+}
+
+void BrowseMenu() {
+
+}
+
+void foodMenu() {
+    int userchoice;
+
+    clear();
+
+    std::cout << "[1] Browse Menu\n[2] View Discounts\n[0] Exit\n" << std::endl;
+    std::cout << "Enter your choice here: ";
+    std::cin >> userchoice;
+
+    switch (userchoice) {
+    case 1:
+        std::cout << "Browse Menu Selected" << std::endl;
+        break;
+    case 2:
+        std::cout << "View Discounts selected" << std::endl;
+        break;
+    case 0:
+        exit(0);
+    }
+}
+
+int main() {
+    login();
+    foodMenu();
 }
