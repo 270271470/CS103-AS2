8c65f0d667ac55ca949bde85c79fe70cc6cf1a07
Mauritz <270271470@yoobeestudent.ac.nz>
2023-06-11 15:21:55 +1200

    Adding ASCII Menu Art

--- CS103-AS2/MenuHandler.cpp
+++ CS103-AS2/MenuHandler.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
 #include "MenuHandler.h"
 
 using namespace std;
@@ -6,24 +9,60 @@ using namespace std;
 MenuHandler::MenuHandler(LoginHandler& lh) : lh(lh) {}
 
 void MenuHandler::start() {
+
+    const int consoleWidth = 150; // Set console width
+
+    string asciiArt = R"(
+ _                      _     ____        _            
+| |                    | |   |  _ \      | |           
+| |    _   _ _ __   ___| |__ | |_) |_   _| |_ ___  ___ 
+| |   | | | | '_ \ / __| '_ \|  _ <| | | | __/ _ \/ __|
+| |___| |_| | | | | (__| | | | |_) | |_| | ||  __/\__ \
+|______\__,_|_| |_|\___|_| |_|____/ \__, |\__\___||___/
+                                     __/ |             
+                                    |___/                                    
+)";
+
+    // Calculate max line length
+    istringstream ss(asciiArt);
+    string line;
+    int maxLength = 0;
+    while (getline(ss, line)) {
+        maxLength = max(maxLength, static_cast<int>(line.size()));
+    }
+
+    // Calc left padding
+    int leftPadding = (consoleWidth - maxLength) / 2;
+
+    // Display ASCII art with padding
+    istringstream ss2(asciiArt);
+    while (getline(ss2, line)) {
+        cout << string(leftPadding, ' ') << line << endl;
+    }
+
+    cout << endl << endl;;
+
     string choice;
+
     while (true) {
-        cout << "Are you an existing user?" << endl;
-        cout << "1. Login" << endl;
-        cout << "2. Register" << endl;
-        cout << "3. Exit" << endl;
+        string menu = "Are you an existing user?\n1. Login\n2. Register\n3. Exit";
+        int padding = (consoleWidth - menu.length()) / 2;
+        menu = string(padding, ' ') + menu;
+
+        cout << menu << endl;
         cin >> choice;
+
         if (choice == "2") {
             if (lh.handleRegistration())
-                cout << "Registration Successful!" << endl;
+                cout << string((consoleWidth - 23) / 2, ' ') << "Registration Successful!" << endl;
             else
-                cout << "Registration Failed. User with the same ID already exists." << endl;
+                cout << string((consoleWidth - 51) / 2, ' ') << "Registration Failed. User with the same ID already exists." << endl;
         }
         else if (choice == "1") {
             if (lh.handleLogin())
-                cout << "Login Successful!" << endl;
+                cout << string((consoleWidth - 18) / 2, ' ') << "Login Successful!" << endl;
             else
-                cout << "Login Failed. Incorrect ID or Password." << endl;
+                cout << string((consoleWidth - 37) / 2, ' ') << "Login Failed. Incorrect ID or Password." << endl;
         }
         else if (choice == "3") {
             break;
