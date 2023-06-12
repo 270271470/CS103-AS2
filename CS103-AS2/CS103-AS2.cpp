7e46fa212e360d2961637a9440fa7efd5a8b652f
JM <200871010@yoobeestudent.ac.nz>
2023-06-13 09:41:31 +1200

    heeelp

---
+++ CS103-AS2/CS103-AS2.cpp
@@ -0,0 +1,14 @@
+// CS103-AS2.cpp : School Lunch Ordering System
+
+// This is our driver code.
+// Because we are using a modular approach, the code in this file is kept to a minimum.
+
+#include "MainMenu.h"
+
+int main() {
+
+    // Call displayMainMenu declared in MainMenu.h
+    displayMainMenu();
+    
+    return 0;
+}
