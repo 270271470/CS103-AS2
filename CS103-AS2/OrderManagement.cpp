a49394e7b19df868e05351173e2b4f43a8744f76
JM <200871010@yoobeestudent.ac.nz>
2023-06-13 10:59:16 +1200

    Added a loop for order; pass 1 Replace bill.csv to bill.txt

--- CS103-AS2/OrderManagement.cpp
+++ CS103-AS2/OrderManagement.cpp
@@ -8,37 +8,42 @@
 using namespace std;
 
 void orderLunch(const User& user) {
-    cout << "Please choose an option:\n";
-    cout << "1. Hamburger ($5)\n";
-    cout << "2. Hotdog ($3)\n";
-    cout << "3. Pizza ($7)\n";
-
     int choice;
-    cin >> choice;
+    do {
+        cout << "\nPlease choose an option:\n";
+        cout << "1. Hamburger ($5)\n";
+        cout << "2. Hotdog ($3)\n";
+        cout << "3. Pizza ($7)\n";
+        cout << "4. End Order\n";
 
-    string item;
-    int price;
-    switch (choice) {
-    case 1:
-        item = "Hamburger";
-        price = 5;
-        break;
-    case 2:
-        item = "Hotdog";
-        price = 3;
-        break;
-    case 3:
-        item = "Pizza";
-        price = 7;
-        break;
-    default:
-        cout << "Invalid option!\n";
-        return;
-    }
+        cout << "Enter your choice here: ";
+        cin >> choice;
 
-    ofstream file("orderdb.csv", ios::app);
-    file << user.id << ',' << user.firstName << ',' << user.lastName << ',' << user.role << ',' << item << ',' << price << '\n';
-    cout << "Order placed for " << item << " for " << price << " dollars.\n";
+        string item;
+        int price;
+        switch (choice) {
+        case 1:
+            item = "Hamburger";
+            price = 5;
+            break;
+        case 2:
+            item = "Hotdog";
+            price = 3;
+            break;
+        case 3:
+            item = "Pizza";
+            price = 7;
+            break;
+        default:
+            cout << "Invalid option!\n";
+            return;
+        }
+
+        ofstream file("orderdb.csv", ios::app);
+        file << user.id << ',' << user.firstName << ',' << user.lastName << ',' << user.role << ',' << item << ',' << price << '\n';
+        cout << "\nOrder placed for " << item << " for " << price << " dollars.\n";
+    } while (choice != 4);
+    
 }
 
 void userMenu(const User& user) {
